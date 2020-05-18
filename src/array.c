d01e2131b9a1cc425352420620bd5ab4ca81e919
Rick Mark <rickmark@outlook.com>
2020-05-18 15:06:05 -0700

    Array code

---
+++ src/array.c
@@ -0,0 +1,227 @@
+/*
+ * array.c - Simple C arrays
+ *
+ * Written in 2020 by Rick Mark <rickmark@outlook.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to
+ * the public domain worldwide. This software is distributed without
+ * any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "array.h"
+
+size_t memory_align_ceiling(size_t size) {
+    size_t aligned = 2;
+    if (size <= 1) return 1;
+    size--;
+    while (size >>= 1 != 0) aligned <<= 1;
+    return aligned;
+}
+
+array_t array_new(size_t elements) {
+    array_t result = calloc(1, sizeof(array_header_t));
+    result->capacity = memory_align_ceiling(elements);
+    result->count = 0;
+    result->data = calloc(result->capacity, sizeof(void*));
+
+    return result;
+}
+
+array_t array_copy(array_t source) {
+    array_t result = malloc(sizeof(array_header_t));
+    memcpy(result, source, sizeof(array_header_t));
+
+    result->data = calloc(source->capacity, sizeof(void*));
+    memcpy(result->data, source->data, source->capacity * sizeof(void*));
+
+    return result;
+}
+
+array_t array_from_split_string(char* string, const char* split_on)
+{
+    array_t items = array_new(8);
+    if (string == NULL) { return items; }
+
+    char* state = NULL;
+    char* local_string = strdup(string);
+    char* token = strtok_r(local_string, split_on, &state);
+
+    while (token != NULL) {
+        array_add(items, strdup(token));
+        token = strtok_r(NULL, split_on, &state);
+    }
+    free(local_string);
+
+    return items;
+}
+
+array_t array_join(array_t first, array_t second) {
+    array_t items = array_new(first->count + second->count);
+
+    void* data_region = (void*)items->data;
+    memcpy(data_region, first->data, first->count * sizeof(void*));
+
+    void* second_region = data_region + (sizeof(void*) * first->count);
+    memcpy(second_region, second->data, second->count * sizeof(void*));
+
+    items->count = first->count + second->count;
+
+    return items;
+}
+
+void array_append_elements(array_t array, array_t to_add) {
+    size_t new_count = array->count + to_add->count;
+    size_t new_capacity = memory_align_ceiling(new_count);
+
+    array->data = realloc(array->data, new_capacity * sizeof(void*));
+    array->capacity = new_capacity;
+
+    void* data_region = (void*)array->data;
+    void* second_region = data_region + (sizeof(void*) * array->count);
+    memcpy(second_region, to_add->data, to_add->count * sizeof(void*));
+
+    array->count = new_count;
+}
+
+void array_append_duplicating_strings(array_t array, array_t to_add) {
+    for (size_t index = 0; index < to_add->count; index++) {
+        array_add(array, strdup(array_element(to_add, index)));
+    }
+}
+
+void array_free(array_t array) {
+    if (array) {
+        if (array->data) {
+            free(array->data);
+        }
+        free(array);
+    }
+}
+
+void array_free_all(array_t array) {
+    for (size_t index = 0; index < array->count; index++) {
+        void* element = array_element(array, index);
+        if (element) { free(element); }
+    }
+
+    array_free(array);
+}
+
+size_t array_count(array_t array) {
+    return array->count;
+}
+
+void array_add(array_t array, void* element) {
+    if (array_is_full(array)) {
+        size_t new_capacity = array->capacity * 2;
+        array->data = realloc(array->data, new_capacity * sizeof(void*));
+        array->capacity = new_capacity;
+    }
+
+    array->data[array->count++] = element;
+}
+
+bool array_is_full(array_t array) {
+    return array->capacity == array->count;
+}
+
+bool array_is_empty(array_t array) {
+    return array->count == 0;
+}
+
+void* array_element(array_t array, size_t index) {
+    if (index > array->count) {
+        return NULL;
+    }
+
+    return array->data[index];
+}
+
+bool array_contains_element(array_t array, void* element) {
+    for (size_t index = 0; index < array->count; index++) {
+        if (array->data[index] == element) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool array_contains_string(array_t array, char* string) {
+    size_t outer_length = strlen(string);
+    for (size_t index = 0; index < array->count; index++) {
+        size_t inner_length = strlen(array->data[index]);
+        if (outer_length != inner_length) {
+            continue;
+        }
+
+        if (memcmp(array->data[index], (void*)string, outer_length)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool array_contains_any_element(array_t array, array_t elements) {
+    for (size_t index = 0; index < array->count; index++) {
+        for (size_t element = 0; element < elements->count; element++) {
+            if (array->data[index] == elements->data[element]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool array_contains_any_string(array_t array, array_t strings) {
+    for (size_t index = 0; index < strings->count; index++) {
+        if (array_contains_string(array, strings->data[index])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool array_string_contains_any(array_t array, char* string) {
+    for (size_t index = 0; index < array->count; index++) {
+        if (strstr(string, array->data[index])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#ifndef STRIP_DEBUG_CODE
+void array_print_debug(char* name, array_t array) {
+    if (array == NULL) {
+        printf("%s = NULL\n", name);
+        return;
+    }
+
+    if (array_is_empty(array)) {
+        printf("%s = empty\n", name);
+        return;
+    }
+
+    printf("%s = (count = %zu, capacity = %zu) [ ", name, array_count(array), array->capacity);
+    for (size_t index = 0; index < array->count; index++) {
+        printf("\"%s\"", array_element(array, index));
+        if (index + 1 != array->count) {
+            printf(", ");
+        }
+    }
+    printf(" ] \n");
+}
+#endif
---
+++ src/array.h
@@ -0,0 +1,220 @@
+/*
+ * array.h - Simple C arrays
+ *
+ * Written in 2020 by Rick Mark <rickmark@outlook.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to
+ * the public domain worldwide. This software is distributed without
+ * any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#ifndef IDEVICECOMMON_ARRAY_H
+#define IDEVICECOMMON_ARRAY_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    size_t capacity;
+    size_t count;
+    void** data;
+} array_header_t;
+
+typedef array_header_t* array_t;
+
+/**
+ * Creates a new array_t with an initial capacity.
+ *
+ * @param capacity The initial capacity of the Array.
+ *
+ * @return a new array.
+ */
+array_t array_new(size_t capacity);
+
+
+/**
+ * Creates a new array_t by copying an array, without copying elements.
+ *
+ * @param source The array to copy.
+ *
+ * @return a new array, which is a copy of the other pointing at the same elements.
+ */
+array_t array_copy(array_t source);
+
+
+/**
+ * Creates a new array_t with elements which are the strings split by a token.
+ *
+ * @param string The string to split.
+ * @param split_on the delineator between strings.
+ *
+ * @return a new array_t with elements which are split from the string.
+ */
+array_t array_from_split_string(char* string, const char* split_on);
+
+
+/**
+ * Creates a new array_t by joining the elements of two array_ts without copying the elements.
+ *
+ * @param first The first array to join.
+ * @param second The second array to join.
+ *
+ * @return a new array_t with the elements of both arrays.
+ */
+array_t array_join(array_t first, array_t second);
+
+
+/**
+ * Frees an array_t without freeing the elements in the array.
+ *
+ * @param array The array to free.
+ */
+void array_free(array_t array);
+
+
+/**
+ * Frees an array_t without freeing the elements in the array.
+ *
+ * @param array The array to free, after freeing all of it's elements.
+ */
+void array_free_all(array_t array);
+
+
+/**
+ * Get's the count of the elements of the array.
+ *
+ * @param array The array to get the count from.
+ *
+ * @return The count of elements in the array.
+ */
+size_t array_count(array_t array);
+
+
+/**
+ * Appends an element to the end of the array.  Will automatically grow the
+ * array's capacity if it is needed.
+ *
+ * @param array The array to be added too.
+ * @param element The element to add to the array.
+ */
+void array_add(array_t array, void* element);
+
+
+/**
+ * Appends elements from one array to the other, without copying or taking
+ * ownership of the elements of the source array.
+ *
+ * @param array The array to append the elements too.
+ * @param to_add The array of elements to append.
+ */
+void array_append_elements(array_t array, array_t to_add);
+
+
+/**
+ * Appends an array to an array by duplicating the elements as strings.
+ *
+ * @param array The array to copy into.
+ * @param to_add An array of strings to append.
+ */
+void array_append_duplicating_strings(array_t array, array_t to_add);
+
+
+/**
+ * Gets an element of the array by index.
+ *
+ * @param array The array to index into.
+ * @param index The index into the array.
+ *
+ * @return The element at the index in the array.
+ */
+void* array_element(array_t array, size_t index);
+
+
+/**
+ * A boolean representing if the array has no elements.
+ *
+ * @param array The array.
+ *
+ * @return True if the array has no elements, otherwise false.
+ */
+bool array_is_empty(array_t array);
+
+
+/**
+ * A boolean representing if the array_t is full, and appending would
+ * cause a reallocation of the backing data.
+ *
+ * @param array The array to check.
+ *
+ * @return True if the array size is at capacity, otherwise false.
+ */
+bool array_is_full(array_t array);
+
+
+/**
+ * A boolean representing if the pointer element is contained in the
+ * array.
+ *
+ * @param array The array to search.
+ * @param element The element to search for.
+ *
+ * @return True if the element is contained in the array.
+ */
+bool array_contains_element(array_t array, void* element);
+
+
+/**
+ * A boolean representing if the string is any of the elements in the
+ * array.
+ *
+ * @param array The array to search.
+ * @param string The string to search for in the array.
+ *
+ * @return True if the string is contained in the array, false if the
+ *         string is not in the array.
+ */
+bool array_contains_string(array_t array, char* string);
+
+
+/**
+ * Searches one array if it contains any of the elements of the second.
+ *
+ * @param array The array to search.
+ * @param elements The elements to search for.
+ *
+ * @return True if array contains an element that is in the array elements.
+ */
+bool array_contains_any_element(array_t array, array_t elements);
+
+
+/**
+ * Search an array elements for any string in array of strings.
+ *
+ * @param array The array to search.
+ * @param strings The strings to search for.
+ *
+ * @return True if the array contains any of the strings in the array strings.
+ */
+bool array_contains_any_string(array_t array, array_t strings);
+
+
+/**
+ * Checks to see if a string contains any of the strings in an array of strings.
+ *
+ * @param array The array of elements to search for.
+ * @param string The string to be searched.
+ */
+bool array_string_contains_any(array_t array, char* string);
+
+#ifndef STRIP_DEBUG_CODE
+void array_print_debug(char* name, array_t array);
+#else
+inline void array_print_debug(char* name, array_t array) {}
+#endif
+
+#endif //IDEVICECOMMON_ARRAY_H
